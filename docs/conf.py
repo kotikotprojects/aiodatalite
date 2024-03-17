2ffa8378992dd70a5d26084d441a4562379a7cd0
hhh <h@localhost>
2024-03-17 16:05:14 +0200

    Developing docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@ sys.path.insert(0, os.path.abspath("../."))
 # -- Project information -----------------------------------------------------
 
 project = "Datalite"
-copyright = "2020, Ege Ozkan"
-author = "Ege Ozkan"
+copyright = "2020, Ege Ozkan; 2024, hhh"
+author = "Ege Ozkan, hhh"
 
 # The full version, including alpha/beta/rc tags
 release = "v0.7.1"
@@ -48,7 +48,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
